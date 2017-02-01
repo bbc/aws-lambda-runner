c69a02a4271e86fc388390b77e774da6d8a4a34f
Tom Rosier <tom.rosier@bbc.co.uk>
2017-02-01 14:02:30 +0000

    remove unused variable

--- js/request.js
+++ js/request.js
@@ -150,9 +150,6 @@ exports.request = function(req, res, opts, handler) {
       var terminationMessage = 'Terminating server at http://[localhost]:' + opts.port + ' for ' + opts['module-path'] + ' / ' + opts.handler;
       res.end(terminationMessage + '\n');
       console.info(terminationMessage);
-      if (server) {
-        server.close();
-      }
     })();
 
   } else if (req.method === 'GET') {
