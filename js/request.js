19dc3b71162eda5907a6af2c46d2021fc62c0d8c
Tom Rosier <tom.rosier@bbc.co.uk>
2017-02-01 13:56:35 +0000

    added error checking to see if the server we are closing is defined

--- js/request.js
+++ js/request.js
@@ -150,7 +150,9 @@ exports.request = function(req, res, opts, handler) {
       var terminationMessage = 'Terminating server at http://[localhost]:' + opts.port + ' for ' + opts['module-path'] + ' / ' + opts.handler;
       res.end(terminationMessage + '\n');
       console.info(terminationMessage);
-      server.close();
+      if (server) {
+        server.close();
+      }
     })();
 
   } else if (req.method === 'GET') {
