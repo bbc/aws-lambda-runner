d27df8155921283b2b3d65a9b39ce144b45311a1
Gary Macindoe <gary.macindoe@bbc.co.uk>
2017-05-10 16:38:28 +0100

    MEDIASERVICES-12079: Fix Ruby unit tests

--- spec/lambdarunner_runner_spec.rb
+++ spec/lambdarunner_runner_spec.rb
@@ -5,7 +5,7 @@ describe LambdaRunner::Runner do
 
   before(:all) do
     test_js = File.expand_path("test.js", File.dirname(__FILE__))
-    @under_test = LambdaRunner::Runner.new(test_js, "node_0_10_42_handler")
+    @under_test = LambdaRunner::Runner.new(test_js, "node_4_3_handler")
     @under_test.start
   end
 
@@ -13,12 +13,12 @@ describe LambdaRunner::Runner do
     @under_test.stop
   end
 
-  it "should emulate AWS Lambda (node.js v0.10.42) - success" do
+  it "should emulate AWS Lambda (node.js v4.3) - success" do
     r = @under_test.process_event({ succeed: { delay: 50, result: "ohai" } })
     expect(r).to eq("ohai")
   end
 
-  it "should emulate AWS Lambda (node.js v0.10.42) - failure" do
+  it "should emulate AWS Lambda (node.js v4.3) - failure" do
     expect {
       @under_test.process_event({ fail: { delay: 50, err: "oh noes" } })
     }.to raise_error("oh noes")
--- spec/test.js
+++ spec/test.js
@@ -1,11 +1,11 @@
-module.exports.node_0_10_42_handler = function (event, context) {
+module.exports.node_4_3_handler = function (event, context, callback) {
   console.log("test handler received event =", JSON.stringify(event));
 
   if (event.succeed) {
     var delay = event.succeed.delay || 0;
     var result = event.succeed.result || null;
     setTimeout(function () {
-      context.done(null, result);
+      callback(null, result);
     }, delay);
   }
 
@@ -13,7 +13,7 @@ module.exports.node_0_10_42_handler = function (event, context) {
     var delay = event.fail.delay || 0;
     var err = event.fail.err || true;
     setTimeout(function () {
-      context.done(err);
+      callback(err);
     }, delay);
   }
 };
